d9ccc422c076a3d9e20434283e1a841af8bb1514
Anna Toll <anna.toll@protonmail.com>
2023-06-02 22:42:15 +0200

    Added code to get csrf cookie

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -42,32 +42,34 @@ const useFetch = (urlEndPoint: string, dataInitState: ResponseTypes = [], onSucc
     return data
   }
 
-  // const getCookie = (name: string) => {
-  //   let cookieValue = null
-  //   if (document.cookie && document.cookie !== '') {
-  //     const cookies = document.cookie.split(';')
-  //     for (let i = 0; i < cookies.length; i++) {
-  //       const cookie = cookies[i].trim()
-  //       // Does this cookie string begin with the name we want?
-  //       if (cookie.substring(0, name.length + 1) === (name + '=')) {
-  //         cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
-  //         break
-  //       }
-  //     }
-  //   }
-  //   return cookieValue
-  // }
+  const getCookie = (name: string) => {
+    let cookieValue = null
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';')
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim()
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+          break
+        }
+      }
+    }
+    return cookieValue
+  }
 
   const baseFetch = async (method: Method = 'GET', body: Record<string, unknown>, whenDoneFn: () => void) => {
     setIsLoading(true)
     setError('')
+
+    const csrftoken = getCookie('csrftoken')
 
     const fetchSettings: any = {
       method,
       credentials: 'include',
       ...(method !== 'GET' && {
         headers: {
-        // ...(csrftoken && { 'X-CSRFToken': csrftoken }),
+          ...(csrftoken && { 'X-CSRFToken': csrftoken }),
           Accept: 'application/json',
           'Content-Type': 'application/json'
         }
